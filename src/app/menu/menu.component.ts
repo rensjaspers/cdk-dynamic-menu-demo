5502de3553b7336c8b6413e0b73f4fc51d09328d
Rens Jaspers <rens.jaspers@gmail.com>
2022-09-11 19:26:20 +0200

    Add type

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, ViewChild } from '@angular/core';
+import { Component, Input, TemplateRef, ViewChild } from '@angular/core';
 import { MenuItem } from './../menu-item.interface';
 
 @Component({
@@ -8,7 +8,7 @@ import { MenuItem } from './../menu-item.interface';
 })
 export class MenuComponent {
   @Input() items!: MenuItem[];
-  @ViewChild('menu', { static: true }) menu: any;
+  @ViewChild('menu', { static: true }) menu!: TemplateRef<any>;
 
   hasChildren(item: MenuItem) {
     return item.children && item.children.length > 0;
