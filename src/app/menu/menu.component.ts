8112f8892688d3f300e4d876e9f9cda21084ae85
Rens Jaspers <rens.jaspers@gmail.com>
2022-09-11 19:23:10 +0200

    Remove unused method

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { Component, Input, ViewChild } from '@angular/core';
 import { MenuItem } from './../menu-item.interface';
 
 @Component({
@@ -6,13 +6,9 @@ import { MenuItem } from './../menu-item.interface';
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss'],
 })
-export class MenuComponent implements OnInit {
+export class MenuComponent {
   @Input() items!: MenuItem[];
   @ViewChild('menu', { static: true }) menu: any;
-
-  constructor() {}
-
-  ngOnInit() {}
 
   hasChildren(item: MenuItem) {
     return item.children && item.children.length > 0;
